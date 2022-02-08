06466f34616a6e2fd7affcde75b798397846e420
tkarasyova <tkarasyova@griddynamics.com>
2022-02-08 17:28:47 +0000

    🦉 Updates from OwlBot
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md

--- samples/interactive-tutorials/product/import_products_bq_test.py
+++ samples/interactive-tutorials/product/import_products_bq_test.py
@@ -18,9 +18,7 @@ import subprocess
 
 def test_import_products_bq():
     output = str(
-        subprocess.check_output(
-            "python import_products_big_query_table.py", shell=True
-        )
+        subprocess.check_output("python import_products_big_query_table.py", shell=True)
     )
 
     assert re.match(".*import products from big query table request.*", output)
