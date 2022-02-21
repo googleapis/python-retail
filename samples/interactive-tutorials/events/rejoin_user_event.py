2123eb722eae56b575db68ce24dc37ea8c633971
tetiana-karasova <tetiana.karasova@gmail.com>
2022-02-21 11:43:26 +0100

    lint fix

--- samples/interactive-tutorials/events/rejoin_user_event.py
+++ samples/interactive-tutorials/events/rejoin_user_event.py
@@ -19,9 +19,9 @@
 import os
 
 from google.api_core.client_options import ClientOptions
-from google.cloud.retail import UserEventServiceClient, RejoinUserEventsRequest
+from google.cloud.retail import RejoinUserEventsRequest, UserEventServiceClient
 
-from setup.setup_cleanup import write_user_event, purge_user_event
+from setup.setup_cleanup import purge_user_event, write_user_event
 
 project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
 
